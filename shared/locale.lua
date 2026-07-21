9ba4460b61bc7072175a8257429b4b7a117f75fb
mokusarcu0-lang <mokusarcu0@gmail.com>
2026-07-21 22:28:32 +0200

    Add final files - Localization and .gitignore

--- shared/locale.lua
+++ shared/locale.lua
@@ -1,125 +1,73 @@
 Locales = {}
 
-Locales.hu = {
-    -- General
-    ['no_permission'] = 'Nincs jogod ehhez!',
-    ['error'] = 'Hiba',
-    ['success'] = 'Siker',
-    ['loading'] = 'Betöltés...',
+Locales['hu'] = {
+    -- Police Job
+    not_police = 'Nem vagy rendőr!',
+    police_duty_on = 'Munkába álltál',
+    police_duty_off = 'Felszabadultál',
     
     -- Dispatch
-    ['dispatch_created'] = 'Hívás létrehozva',
-    ['dispatch_description'] = 'Hívás típusa: %s',
-    ['dispatch_gps'] = 'GPS beállítva',
-    ['dispatch_taken'] = '%s felvette a hívást',
-    ['dispatch_closed'] = 'Hívás lezárva',
-    ['dispatch_list'] = 'Aktív hívások',
-    ['no_dispatch'] = 'Nincsenek aktív hívások',
-    
-    -- Police
-    ['police_duty_on'] = 'Szolgálatba állás',
-    ['police_duty_off'] = 'Szolgálatból kiállás',
-    ['already_on_duty'] = 'Már szolgálatban vagy!',
-    ['already_off_duty'] = 'Nem vagy szolgálatban!',
-    ['not_police'] = 'Nem vagy rendőr!',
+    dispatch_created = 'Új hívás létrehozva!',
+    dispatch_closed = 'Hívás lezárva!',
+    dispatch_gps = 'GPS beállítva a célpontra',
+    dispatch_list = 'Aktív Hívások',
     
     -- Wanted
-    ['set_wanted'] = 'Körözés beállítva: %s (csillag: %d)',
-    ['remove_wanted'] = 'Körözés eltávolítva: %s',
-    ['wanted_level'] = 'Körözési szint: %d csillag',
+    set_wanted = '%s játékos körözve: %d csillag',
     
     -- Jail
-    ['jail_player'] = '%s börtönbe került (%d mp)',
-    ['unjail_player'] = '%s szabadult',
-    ['jail_not_available'] = 'Nincs szabad cella!',
-    ['jail_remaining'] = 'Hátralevő idő: %d másodperc',
-    ['released_from_jail'] = 'Szabadulás börtönből',
+    jail_player = '%s játékost %d másodpercre börtönbe helyezted',
+    released_from_jail = 'Kibocsátva a börtönből!',
+    jail_remaining = 'Börtön időd: %d másodperc',
+    unjail_player = '%s játékost felszabadítottad',
     
     -- MDT
-    ['mdt_title'] = 'Mobil Adatopó',
-    ['mdt_vehicle_info'] = 'Jármű információ',
-    ['mdt_driver_info'] = 'Sofőr információ',
-    ['mdt_warrants'] = 'Elfogatási parancsok',
-    ['mdt_licenses'] = 'Jogosítványok',
-    ['mdt_notes'] = 'Megjegyzések',
-    ['mdt_dispatch'] = 'Hívások',
+    mdt_title = 'Mobil Adatopó',
+    
+    -- General
+    error = 'Hiba',
+    success = 'Siker',
     
     -- Commands
-    ['cmd_dispatch'] = 'Hívás létrehozása',
-    ['cmd_dispatch_help'] = '/dispatch [típus] [leírás]',
-    ['cmd_jail'] = 'Játékos börtönbe zárása',
-    ['cmd_jail_help'] = '/jail [id] [idő(mp)]',
-    ['cmd_unjail'] = 'Játékos szabadítása',
-    ['cmd_unjail_help'] = '/unjail [id]',
-    ['cmd_wanted'] = 'Körözés beállítása',
-    ['cmd_wanted_help'] = '/wanted [id] [csillag(1-5)]',
-    ['cmd_mdt'] = 'MDT megnyitása',
-    ['cmd_duty'] = 'Szolgálat ki/be',
+    cmd_jail_help = 'Használat: /jail [PlayerID] [Idő másodpercben]',
+    cmd_unjail_help = 'Használat: /unjail [PlayerID]',
+    cmd_wanted_help = 'Használat: /wanted [PlayerID] [1-5 csillag]'
 }
 
-Locales.en = {
-    -- General
-    ['no_permission'] = 'You don\'t have permission!',
-    ['error'] = 'Error',
-    ['success'] = 'Success',
-    ['loading'] = 'Loading...',
+Locales['en'] = {
+    -- Police Job
+    not_police = 'You are not a police officer!',
+    police_duty_on = 'You are now on duty',
+    police_duty_off = 'You are now off duty',
     
     -- Dispatch
-    ['dispatch_created'] = 'Call created',
-    ['dispatch_description'] = 'Call type: %s',
-    ['dispatch_gps'] = 'GPS set',
-    ['dispatch_taken'] = '%s took the call',
-    ['dispatch_closed'] = 'Call closed',
-    ['dispatch_list'] = 'Active calls',
-    ['no_dispatch'] = 'No active calls',
-    
-    -- Police
-    ['police_duty_on'] = 'On duty',
-    ['police_duty_off'] = 'Off duty',
-    ['already_on_duty'] = 'Already on duty!',
-    ['already_off_duty'] = 'Not on duty!',
-    ['not_police'] = 'You are not a police officer!',
+    dispatch_created = 'New dispatch call created!',
+    dispatch_closed = 'Dispatch call closed!',
+    dispatch_gps = 'GPS set to location',
+    dispatch_list = 'Active Calls',
     
     -- Wanted
-    ['set_wanted'] = 'Wanted set: %s (stars: %d)',
-    ['remove_wanted'] = 'Wanted removed: %s',
-    ['wanted_level'] = 'Wanted level: %d stars',
+    set_wanted = '%s player wanted: %d stars',
     
     -- Jail
-    ['jail_player'] = '%s jailed (%d sec)',
-    ['unjail_player'] = '%s released',
-    ['jail_not_available'] = 'No available cells!',
-    ['jail_remaining'] = 'Time remaining: %d seconds',
-    ['released_from_jail'] = 'Released from jail',
+    jail_player = 'Player %s jailed for %d seconds',
+    released_from_jail = 'Released from jail!',
+    jail_remaining = 'Jail time remaining: %d seconds',
+    unjail_player = 'Player %s has been released',
     
     -- MDT
-    ['mdt_title'] = 'Mobile Data Terminal',
-    ['mdt_vehicle_info'] = 'Vehicle Info',
-    ['mdt_driver_info'] = 'Driver Info',
-    ['mdt_warrants'] = 'Warrants',
-    ['mdt_licenses'] = 'Licenses',
-    ['mdt_notes'] = 'Notes',
-    ['mdt_dispatch'] = 'Calls',
+    mdt_title = 'Mobile Data Terminal',
+    
+    -- General
+    error = 'Error',
+    success = 'Success',
     
     -- Commands
-    ['cmd_dispatch'] = 'Create a dispatch call',
-    ['cmd_dispatch_help'] = '/dispatch [type] [description]',
-    ['cmd_jail'] = 'Jail a player',
-    ['cmd_jail_help'] = '/jail [id] [time(sec)]',
-    ['cmd_unjail'] = 'Release a player',
-    ['cmd_unjail_help'] = '/unjail [id]',
-    ['cmd_wanted'] = 'Set wanted level',
-    ['cmd_wanted_help'] = '/wanted [id] [stars(1-5)]',
-    ['cmd_mdt'] = 'Open MDT',
-    ['cmd_duty'] = 'Toggle duty',
+    cmd_jail_help = 'Usage: /jail [PlayerID] [Time in seconds]',
+    cmd_unjail_help = 'Usage: /unjail [PlayerID]',
+    cmd_wanted_help = 'Usage: /wanted [PlayerID] [1-5 stars]'
 }
 
-function GetLocale(key)
-    local locale = Locales[Config.Locale] or Locales.en
-    return locale[key] or 'Unknown translation: ' .. key
-end
-
-function _U(key, ...)
-    local str = GetLocale(key)
-    return string.format(str, ...)
+function _U(str)
+    return Locales[Config.Locale][str]
 end
